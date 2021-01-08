a48d0642179e91417b5558ec6a404650198d7210
Hongming <hongming.li@mail.utoronto.ca>
2021-01-08 00:14:29 -0500

    deleted some useless code

--- src/components/Header/Header.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-
-import Header from "./Header"
-
-const story = storiesOf("Components/Header", module)
-
-story.add("Header", () => <Header siteTitle={"sample"} />)
