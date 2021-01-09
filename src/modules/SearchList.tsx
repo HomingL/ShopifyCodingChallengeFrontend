4347ed0abb4be5e59c60721815c72b882b264dbe
Hongming <hongming.li@mail.utoronto.ca>
2021-01-08 20:25:22 -0500

    Added movie detail

--- src/modules/SearchList.tsx
+++ src/modules/SearchList.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import BoxLayout from "../components/BoxLayout"
-import { Typography, Grid, IconButton, Theme } from "@material-ui/core"
+import { Typography, Grid, IconButton, Theme, DialogTitle, DialogContent } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import AddIcon from '@material-ui/icons/Add';
 import { Movie } from "src/types/movie"
+import { Dialog } from '@material-ui/core';
+
 
 
 type SearchListBoxProps = {
@@ -16,8 +18,6 @@ type SearchListBoxProps = {
 
 export const SearchListBox: FC<SearchListBoxProps> = ({ text, movies, onAdd, nominations, response }) => {
   let nominationTitles = nominations.map(nomination => nomination.Title)
-  console.log(response)
-
   return (
     <BoxLayout>
       <Typography variant={"h5"}> Results for "{text}" </Typography>
@@ -36,8 +36,6 @@ type SearchListProps = {
 const SearchList: FC<SearchListProps> = ({ movies, onAdd, nominationTitles, response }) => {
 
   // movies != undefined ? console.log(movies[0].Title) : console.log("length 0")
-  const classes = useStyles();
-
   const handleAdd = (index: number) => {
     onAdd(movies[index]);
   }
@@ -81,22 +79,50 @@ type SearchListItemProps = {
 
 const SearchListItem: FC<SearchListItemProps> = ({ movie, index, displayAdd, onAdd }) => {
   const classes = useStyles()
+  const [open, setOpen] = useState(false)
+
   const handleAdd = (index: number) => {
     onAdd(index)
-  } 
+  }
+  const handleOpen = () => setOpen(true)
 
   return (
     <div className={classes.skill}>
-      <Typography variant="subtitle1" className={classes.title} color="primary">
+      <Typography variant="subtitle1" className={classes.title} color="primary" onClick={handleOpen}>
         <strong>{movie.Title}  {movie.Year}</strong>
       </Typography>
-      
+
       {displayAdd && (<IconButton onClick={() => handleAdd(index)}>
         <AddIcon />
       </IconButton>) }
+      <SearchListItemDetail open={open} onClose={() => setOpen(false)} movie={movie}/>
     </div>
   )
 }
+
+type SearchListItemDetailProps = {
+  open: boolean
+  onClose: () => void
+  movie: Movie
+}
+
+const SearchListItemDetail: FC<SearchListItemDetailProps> = ({ open, onClose, movie }) => (
+
+  <Dialog open={open} onClose={onClose} maxWidth="xl" >
+    <DialogTitle disableTypography> <Typography> Movie Detail of <b>"{movie.Title}" </b> </Typography> </DialogTitle>
+    <DialogContent> 
+      <Grid container justify={"center"}>
+        <Grid item xs={6}>
+          <Typography variant="subtitle1"> Year: {movie.Year} </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle1"> imdbID: {movie.imdbID}</Typography>
+        </Grid>
+        <Grid item xs={12} > <img src={movie.Poster || undefined}></img></Grid>
+      </Grid>  
+    </DialogContent>
+  </Dialog>
+)
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
   title: {
